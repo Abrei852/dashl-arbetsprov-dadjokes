39a39ff500518c886704033e6159fc428d814bd5
abrei852 <abbe1334@gmail.com>
2021-11-19 22:19:22 +0100

    created component to fetch from api

---
+++ src/Api/index.js
@@ -0,0 +1,17 @@
+async function FetchJokes(setJokes, search) {
+	if (search.length > 0) {
+		await fetch(
+			`https://icanhazdadjoke.com/search?term=${search}`,
+			{
+				method: "GET",
+				headers: {
+					Accept: "application/json",
+				},
+			}
+		)
+			.then((response) => response.json())
+			.then((data) => setJokes(data.results));
+	}
+}
+
+export default FetchJokes;
