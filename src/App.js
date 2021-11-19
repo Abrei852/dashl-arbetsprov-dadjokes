9f22bf33bac814220c9a44f96ff22e72613cd08c
abrei852 <abbe1334@gmail.com>
2021-11-19 22:39:31 +0100

    gets jokes from api & sends data to components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,38 @@
-import './App.css';
+import FetchJokes from "./Api";
+import React, { useState, useEffect } from "react";
+import SearchForm from "./SearchForm";
+import SuggestionBox from "./SuggestionBox";
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      
-    </div>
-  );
+	const [jokes, setJokes] = useState([]);
+	const [search, setSearch] = useState("");
+
+	useEffect(() => {
+		const timer = setTimeout(() => {
+			FetchJokes(setJokes, search);
+		}, 500);
+		return () => clearTimeout(timer);
+	}, [search]);
+
+	const onSearchChange = (text) => {
+		if (text.trim() === "") {
+			setSearch([]);
+			setJokes([]);
+		} else {
+			setSearch(text);
+		}
+	};
+
+	return (
+		<div className="App">
+			<SearchForm
+				search={search}
+				onSearchChange={onSearchChange}
+			/>
+			<SuggestionBox jokes={jokes} />
+		</div>
+	);
 }
 
 export default App;
