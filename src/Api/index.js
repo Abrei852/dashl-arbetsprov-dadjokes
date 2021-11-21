14afbc1b8625c50be72f9625c5d0cc2726721fc4
abrei852 <abbe1334@gmail.com>
2021-11-21 02:29:55 +0100

    moved setState from component bp

--- src/Api/index.js
+++ src/Api/index.js
@@ -1,17 +1,15 @@
-async function FetchJokes(setJokes, search) {
-	if (search.trim().length > 0) {
-		await fetch(
-			`https://icanhazdadjoke.com/search?term=${search}`,
-			{
-				method: "GET",
-				headers: {
-					Accept: "application/json",
-				},
-			}
-		)
-			.then((response) => response.json())
-			.then((data) => setJokes(data.results));
-	}
+async function FetchJokes(search) {
+	return await fetch(
+		`https://icanhazdadjoke.com/search?term=${search}`,
+		{
+			method: "GET",
+			headers: {
+				Accept: "application/json",
+			},
+		}
+	)
+		.then((response) => response.json())
+		.catch((error) => console.log(error));
 }
 
 export default FetchJokes;
